7c524e965f3a2a370e140b0019f772d47571a32a
Kasidit Bualoeng <subdit@gmail.com>
2024-07-24 16:45:42 -0400

    implement onChangHandler on SnippetEditFormPage to take value from the user input, modified ReadMe.md file

--- snippet-app/src/components/snippet-edit-form.tsx
+++ snippet-app/src/components/snippet-edit-form.tsx
@@ -2,11 +2,17 @@
 
 import { Snippet } from '@prisma/client';
 import { Editor } from '@monaco-editor/react';
+import { useState } from 'react';
+
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function SnippetEditFormPage({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = '') => {
+    setCode(value);
+  };
   return (
     <div>
       <Editor
@@ -15,6 +21,7 @@ export default function SnippetEditFormPage({ snippet }: SnippetEditFormProps) {
         language='javascript'
         defaultValue={snippet.code}
         options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
       />
     </div>
   );
