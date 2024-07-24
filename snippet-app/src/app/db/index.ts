0b4c1361327d9cf9bd5d3116bfe476ef742e957b
Kasidit Bualoeng <subdit@gmail.com>
2024-07-23 20:30:26 -0400

    create Prisma schema and create form on snippetCreatePage

---
+++ snippet-app/src/app/db/index.ts
@@ -0,0 +1,11 @@
+import { Prisma } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/extension';
+
+export const db = new PrismaClient();
+
+db.snippet.create({
+  data: {
+    title: 'Title',
+    code: 'const abc = () => {}'
+  }
+});
--- snippet-app/src/app/snippets/new/page.tsx
+++ snippet-app/src/app/snippets/new/page.tsx
@@ -1,7 +1,32 @@
-export default function Newv() {
+export default function SnippetCreatePage() {
   return (
-    <div>
-      <h3>New Pages!</h3>
-    </div>
+    <form>
+      <h3 className='font-bold m-3'>Create Snippet</h3>
+      <div className='flex flex-col gap-4'>
+        <div className='flex gap-4'>
+          <label className='w-12' htmlFor='title'>
+            Title
+          </label>
+          <input
+            name='title'
+            className='border-2 rounded p-2 w-full'
+            id='title'
+          />
+        </div>
+        <div className='flex gap-4'>
+          <label className='w-12' htmlFor='code'>
+            Code
+          </label>
+          <input
+            name='code'
+            className='border-2 rounded p-2 w-full'
+            id='code'
+          />
+        </div>
+        <button type='submit' className='rounded p-2 bg-blue-200'>
+          Create
+        </button>
+      </div>
+    </form>
   );
 }
