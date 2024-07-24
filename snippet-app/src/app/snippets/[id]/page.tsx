614fb8f9beef39dd13c838d96520253e24a44f26
Kasidit Bualoeng <subdit@gmail.com>
2024-07-24 14:16:28 -0400

    hookup Edit button to Link to Edit Page id

--- snippet-app/src/app/snippets/[id]/page.tsx
+++ snippet-app/src/app/snippets/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { db } from '@/app/db';
 import { notFound } from 'next/navigation';
-
+import Link from 'next/link';
 interface SnippetShowPageProps {
   params: {
     id: string;
@@ -20,12 +20,18 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     <div>
       <div className='flex m-4 justify-between items-center '>
         <h1 className='text-xl font-bold'>{snippet.title}</h1>
-        <div>
-          <button className='p-2 border rounded'>Edit</button>
+        <div className='flex gap-4'>
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className='p-2 border rounded'>
+            Edit
+          </Link>
           <button className='p-2 border rounded'>Delete</button>
         </div>
-        <pre></pre>
       </div>
+      <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
+        {snippet.code}
+      </pre>
     </div>
   );
 }
