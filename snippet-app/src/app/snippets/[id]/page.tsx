84cad6f6a1d9fd1d5370b494e685ddb1abe8aaf3
Kasidit Bualoeng <subdit@gmail.com>
2024-07-24 11:09:11 -0400

    created snippet id page to render a dynamic path to snippetCreatePage

---
+++ snippet-app/src/app/snippets/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { db } from '@/app/db';
+import { notFound } from 'next/navigation';
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(props.params.id) }
+  });
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>{snippet.title}</div>;
+}
